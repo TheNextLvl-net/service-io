60f0de59615538ef8b30bf2c3fc4219707d86b44
david <mrminecraft00@gmail.com>
2024-07-21 19:47:12 +0200

    added build file

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("java")
+    id("java-library")
+    id("maven-publish")
+}
+
+group = "net.thenextlvl.services"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.thenextlvl.net/releases")
+    maven("https://papermc.io/repo/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    compileOnly("net.thenextlvl.core:annotations:2.0.1")
+    compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+    repositories.maven {
+        val channel = if ((version as String).contains("-pre")) "snapshots" else "releases"
+        url = uri("https://repo.thenextlvl.net/$channel")
+        credentials {
+            username = System.getenv("REPOSITORY_USER")
+            password = System.getenv("REPOSITORY_TOKEN")
+        }
+    }
+}
