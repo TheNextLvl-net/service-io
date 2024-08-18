a2749cc220a4053507ceb8add29bbbcba7baabb9
david <mrminecraft00@gmail.com>
2024-08-18 18:46:40 +0200

    Add Vault support and version checker
    Integrated Vault chat and permission hooks including GroupManager and LuckPerms configurations. Added a version checker and initial metrics setup for plugin analytics. Enhanced onEnable and onDisable methods to manage newly added functionalities.

--- plugin/src/main/java/net/thenextlvl/services/ServicePlugin.java
+++ plugin/src/main/java/net/thenextlvl/services/ServicePlugin.java
@@ -3,6 +3,8 @@ package net.thenextlvl.services;
 import com.google.common.base.Preconditions;
 import lombok.Getter;
 import lombok.experimental.Accessors;
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.permission.Permission;
 import net.thenextlvl.services.api.ServiceProvider;
 import net.thenextlvl.services.api.capability.CapabilityController;
 import net.thenextlvl.services.api.chat.ChatController;
@@ -10,27 +12,60 @@ import net.thenextlvl.services.api.economy.EconomyController;
 import net.thenextlvl.services.api.permission.GroupController;
 import net.thenextlvl.services.capability.PaperCapabilityController;
 import net.thenextlvl.services.chat.PaperChatController;
+import net.thenextlvl.services.hook.chat.VaultChatGroupManager;
+import net.thenextlvl.services.hook.chat.VaultChatLuckPerms;
+import net.thenextlvl.services.hook.permission.VaultGroupManager;
+import net.thenextlvl.services.hook.permission.VaultLuckPerms;
+import net.thenextlvl.services.hook.permission.VaultSuperPerms;
 import net.thenextlvl.services.listener.PluginListener;
+import net.thenextlvl.services.version.PluginVersionChecker;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.Callable;
 
 @Getter
 @Accessors(fluent = true)
 public class ServicePlugin extends JavaPlugin implements ServiceProvider {
     private final Map<Plugin, CapabilityController> capabilityControllers = new HashMap<>();
+    private final PluginVersionChecker versionChecker = new PluginVersionChecker(this);
+    private final Metrics metrics = new Metrics(this, 23083);
+
+    private @Nullable Permission permissions;
 
     @Override
     public void onLoad() {
-        getServer().getServicesManager().register(ServiceProvider.class, this, this, ServicePriority.Highest);
+        versionChecker().checkVersion();
+        registerServices();
     }
 
     @Override
     public void onEnable() {
+        registerListeners();
+
+        loadPermissionControllers();
+        loadChatControllers();
+
+        loadVaultPermission();
+        loadVaultChat();
+    }
+
+    @Override
+    public void onDisable() {
+        metrics().shutdown();
+    }
+
+    private void registerServices() {
+        getServer().getServicesManager().register(ServiceProvider.class, this, this, ServicePriority.Highest);
+    }
+
+    private void registerListeners() {
         getServer().getPluginManager().registerEvents(new PluginListener(this), this);
     }
 
@@ -44,7 +79,7 @@ public class ServicePlugin extends JavaPlugin implements ServiceProvider {
     public ChatController chatController() {
         return Objects.requireNonNullElseGet(
                 getServer().getServicesManager().load(ChatController.class),
-                PaperChatController::new
+                () -> new PaperChatController(this)
         );
     }
 
@@ -62,5 +97,51 @@ public class ServicePlugin extends JavaPlugin implements ServiceProvider {
                 getServer().getServicesManager().load(GroupController.class),
                 "No GroupController available"
         );
+    }
+
+    private void loadPermissionControllers() {
+    }
+
+    private void loadChatControllers() {
+    }
+
+    private void loadVaultChat() {
+        hookVaultChat("GroupManager", () -> new VaultChatGroupManager(this, permissions), ServicePriority.High);
+        hookVaultChat("LuckPerms", () -> new VaultChatLuckPerms(this, permissions), ServicePriority.Highest);
+    }
+
+    private void loadVaultPermission() {
+        hookVaultPermission("GroupManager", () -> new VaultGroupManager(this), ServicePriority.High);
+        hookVaultPermission("LuckPerms", () -> new VaultLuckPerms(this), ServicePriority.Highest);
+
+        var permissions = new VaultSuperPerms(this);
+        getServer().getServicesManager().register(Permission.class, permissions, this, ServicePriority.Lowest);
+        getComponentLogger().info("[Permission] SuperPermissions loaded as backup permission system.");
+
+        this.permissions = getServer().getServicesManager().load(Permission.class);
+    }
+
+    private void hookVaultChat(String name, Callable<? extends Chat> callable, ServicePriority priority) {
+        try {
+            if (getServer().getPluginManager().getPlugin(name) == null) return;
+            var hook = callable.call();
+            getServer().getServicesManager().register(Chat.class, hook, this, priority);
+            getComponentLogger().info("[Chat] Registered Vault support for {}", name);
+        } catch (Exception e) {
+            getComponentLogger().error("[Chat] Failed to register Vault support for {}" +
+                                       " - check to make sure you're using a compatible version!", name, e);
+        }
+    }
+
+    private void hookVaultPermission(String name, Callable<? extends Permission> callable, ServicePriority priority) {
+        try {
+            if (getServer().getPluginManager().getPlugin(name) == null) return;
+            var hook = callable.call();
+            getServer().getServicesManager().register(Permission.class, hook, this, priority);
+            getComponentLogger().info("[Permission] Registered Vault support for {}", name);
+        } catch (Exception e) {
+            getComponentLogger().error("[Permission] Failed to register Vault support for {}" +
+                                       " - check to make sure you're using a compatible version!", name, e);
+        }
     }
 }
