3be97bcdf0353b1ff9dd087eacc35c20cd6643af
david <mrminecraft00@gmail.com>
2024-08-18 18:46:56 +0200

    Add PluginVersionChecker for version management
    This commit introduces the `PluginVersionChecker` class to handle plugin version checks and ensure compatibility. It includes annotations to enforce non-null constraints at the package level and utilizes the `PaperHangarVersionChecker` to compare and report plugin versions.

---
+++ plugin/src/main/java/net/thenextlvl/services/version/PluginVersionChecker.java
@@ -0,0 +1,41 @@
+package net.thenextlvl.services.version;
+
+import core.paper.version.PaperHangarVersionChecker;
+import core.version.SemanticVersion;
+import lombok.Getter;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+
+@Getter
+@SuppressWarnings("UnstableApiUsage")
+public class PluginVersionChecker extends PaperHangarVersionChecker<SemanticVersion> {
+    private final SemanticVersion versionRunning;
+    private final Plugin plugin;
+
+    public PluginVersionChecker(Plugin plugin) {
+        super("ServiceIO");
+        this.plugin = plugin;
+        this.versionRunning = Objects.requireNonNull(parseVersion(plugin.getPluginMeta().getVersion()));
+    }
+
+    @Override
+    public @Nullable SemanticVersion parseVersion(String version) {
+        return SemanticVersion.parse(version);
+    }
+
+    public void checkVersion() {
+        retrieveLatestSupportedVersion(latest -> latest.ifPresentOrElse(version -> {
+            if (version.equals(getVersionRunning())) {
+                plugin.getComponentLogger().info("You are running the latest version of ServiceIO");
+            } else if (version.compareTo(getVersionRunning()) > 0) {
+                plugin.getComponentLogger().warn("An update for ServiceIO is available");
+                plugin.getComponentLogger().warn("You are running version {}, the latest supported version is {}", getVersionRunning(), version);
+                plugin.getComponentLogger().warn("Update at https://hangar.papermc.io/TheNextLvl/ServiceIO");
+            } else {
+                plugin.getComponentLogger().warn("You are running a snapshot version of ServiceIO");
+            }
+        }, () -> plugin.getComponentLogger().error("Version check failed")));
+    }
+}
