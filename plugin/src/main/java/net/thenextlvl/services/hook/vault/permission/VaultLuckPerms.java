30cecf4193e37977bce6bf080e202dbd0806864d
david <mrminecraft00@gmail.com>
2024-08-22 18:11:52 +0200

    Enhance null safety and add user/group retrieval methods
    Implemented methods to retrieve user and group objects with null safety validations. Improved `playerHas` method to handle null inputs for player and permission, ensuring robust permission checks across various contexts.

--- plugin/src/main/java/net/thenextlvl/services/hook/vault/permission/VaultLuckPerms.java
+++ plugin/src/main/java/net/thenextlvl/services/hook/vault/permission/VaultLuckPerms.java
@@ -2,20 +2,27 @@ package net.thenextlvl.services.hook.vault.permission;
 
 import lombok.Getter;
 import net.luckperms.api.LuckPerms;
+import net.luckperms.api.cacheddata.CachedDataManager;
+import net.luckperms.api.cacheddata.CachedMetaData;
 import net.luckperms.api.context.ImmutableContextSet;
+import net.luckperms.api.model.group.Group;
+import net.luckperms.api.model.user.User;
 import net.luckperms.api.query.QueryMode;
 import net.luckperms.api.query.QueryOptions;
 import net.milkbowl.vault.permission.Permission;
 import net.thenextlvl.services.ServicePlugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
+import java.util.Optional;
 
 @Getter
 public class VaultLuckPerms extends Permission {
     private final String name = "LuckPerms";
     private final LuckPerms luckPerms;
 
-    public VaultLuckPerms(ServicePlugin plugin) {
+    public VaultLuckPerms(@NotNull ServicePlugin plugin) {
         var servicesManager = plugin.getServer().getServicesManager();
         this.luckPerms = Objects.requireNonNull(servicesManager.load(LuckPerms.class), "LuckPerms not found");
         this.plugin = plugin;
@@ -31,12 +38,28 @@ public class VaultLuckPerms extends Permission {
         return true;
     }
 
+    private @NotNull Optional<User> getUser(@Nullable String name) {
+        return Optional.ofNullable(name).map(getLuckPerms().getUserManager()::getUser);
+    }
+
+    private @NotNull Optional<Group> getGroup(@Nullable String name) {
+        return Optional.ofNullable(name).map(getLuckPerms().getGroupManager()::getGroup);
+    }
+
+    private @NotNull Optional<CachedMetaData> getMetaData(@NotNull CachedDataManager manager, @Nullable String world) {
+        var options = Optional.ofNullable(world).map(s -> QueryOptions.contextual(ImmutableContextSet.of("world", s)));
+        return options.map(manager::getMetaData).or(() -> Optional.of(manager.getMetaData()));
+    }
+
     @Override
     public boolean playerHas(String world, String player, String permission) {
+        if (player == null || permission == null) return false;
         var user = luckPerms.getUserManager().getUser(player);
-        return user != null && user.getCachedData().getPermissionData(QueryOptions.builder(QueryMode.CONTEXTUAL)
+        if (user == null) return false;
+        return world != null ? user.getCachedData().getPermissionData(QueryOptions.builder(QueryMode.CONTEXTUAL)
                 .context(ImmutableContextSet.of("world", world)).build()
-        ).checkPermission(permission).asBoolean();
+        ).checkPermission(permission).asBoolean() : user.getCachedData().getPermissionData()
+                .checkPermission(permission).asBoolean();
     }
 
     @Override
