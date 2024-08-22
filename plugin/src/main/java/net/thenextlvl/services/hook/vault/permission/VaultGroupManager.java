de1c9954b6ff2edd168b63954c6717091b2b770c
david <mrminecraft00@gmail.com>
2024-08-22 18:11:43 +0200

    Refactor VaultGroupManager permission handling methods
    Simplify the handling of user and group permissions by introducing Optional-based helper methods. This change enhances code readability and maintainability by reducing redundancy and improving null safety checks.

--- plugin/src/main/java/net/thenextlvl/services/hook/vault/permission/VaultGroupManager.java
+++ plugin/src/main/java/net/thenextlvl/services/hook/vault/permission/VaultGroupManager.java
@@ -2,181 +2,143 @@ package net.thenextlvl.services.hook.vault.permission;
 
 import lombok.Getter;
 import net.milkbowl.vault.permission.Permission;
+import net.thenextlvl.services.ServicePlugin;
 import org.anjocaido.groupmanager.GroupManager;
 import org.anjocaido.groupmanager.data.Group;
 import org.anjocaido.groupmanager.data.User;
 import org.anjocaido.groupmanager.dataholder.OverloadedWorldHolder;
-import org.anjocaido.groupmanager.permissions.AnjoPermissionsHandler;
-import org.bukkit.Bukkit;
+import org.anjocaido.groupmanager.dataholder.WorldDataHolder;
 import org.bukkit.generator.WorldInfo;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Objects;
+import java.util.Optional;
 
 @Getter
 public class VaultGroupManager extends Permission {
-    private final String name = "GroupManager";
-    private final GroupManager groupManager;
+    private final @NotNull String name = "GroupManager";
+    private final @NotNull GroupManager groupManager;
 
-    public VaultGroupManager(Plugin plugin) {
+    public VaultGroupManager(@NotNull ServicePlugin plugin) {
         this.groupManager = JavaPlugin.getPlugin(GroupManager.class);
         this.plugin = plugin;
     }
 
-    @Override
-    public boolean playerHas(@Nullable String worldName, @Nullable String playerName, @Nullable String permission) {
-        AnjoPermissionsHandler owh;
-        if (worldName == null) owh = getGroupManager().getWorldsHolder().getWorldPermissionsByPlayerName(playerName);
-        else owh = getGroupManager().getWorldsHolder().getWorldPermissions(worldName);
-        if (owh == null) return false;
-        return owh.permission(playerName, permission);
+    private @NotNull Optional<OverloadedWorldHolder> getWorldHolder(@Nullable String player, @Nullable String world) {
+        return Optional.ofNullable(world)
+                .map(getGroupManager().getWorldsHolder()::getWorldData)
+                .or(() -> Optional.ofNullable(player).map(getGroupManager().getWorldsHolder()::getWorldData));
     }
 
-    private @Nullable User getUser(@Nullable String playerName, @Nullable String worldName) {
-        OverloadedWorldHolder owh;
-        if (worldName == null) owh = getGroupManager().getWorldsHolder().getWorldDataByPlayerName(playerName);
-        else owh = getGroupManager().getWorldsHolder().getWorldData(worldName);
-        if (owh == null) return null;
-        return owh.getUser(playerName);
+    private @NotNull Optional<User> getUser(@Nullable String player, @Nullable String world) {
+        return getWorldHolder(player, world).flatMap(holder -> Optional.ofNullable(player).map(holder::getUser));
     }
 
     @Override
-    public boolean playerAdd(@Nullable String worldName, @Nullable String playerName, @Nullable String permission) {
-        if (playerName == null || worldName == null) return false;
-        var user = getUser(playerName, worldName);
-        if (user == null) return false;
-        user.addPermission(permission);
-        var player = Bukkit.getPlayer(playerName);
-        if (player != null) GroupManager.getBukkitPermissions().updatePermissions(player);
-        return true;
+    public boolean playerHas(@Nullable String world, @NotNull String name, @NotNull String permission) {
+        return getWorldHolder(name, world)
+                .map(WorldDataHolder::getPermissionsHandler)
+                .map(handler -> handler.permission(name, permission))
+                .orElse(false);
     }
 
     @Override
-    public boolean playerRemove(String worldName, String playerName, String permission) {
-        if (playerName == null || worldName == null) return false;
-        var user = getUser(playerName, worldName);
-        if (user == null) return false;
-        user.removePermission(permission);
-        var player = Bukkit.getPlayer(playerName);
-        if (player != null) GroupManager.getBukkitPermissions().updatePermissions(player);
-        return true;
-    }
-
-    private @NotNull OverloadedWorldHolder getWorldHolder(String worldName) {
-        if (worldName == null) return getGroupManager().getWorldsHolder().getDefaultWorld();
-        else return getGroupManager().getWorldsHolder().getWorldData(worldName);
+    public boolean playerAdd(@Nullable String world, @NotNull String name, @NotNull String permission) {
+        return getUser(name, world).map(user -> {
+            user.addPermission(permission);
+            return true;
+        }).orElse(false);
     }
 
     @Override
-    public boolean groupHas(String worldName, String groupName, String permission) {
-        var holder = getWorldHolder(worldName);
-        var group = holder.getGroup(groupName);
-        if (group == null) return false;
-        return group.hasSamePermissionNode(permission);
+    public boolean playerRemove(@Nullable String world, @NotNull String name, @NotNull String permission) {
+        return getUser(name, world)
+                .map(user -> user.removePermission(permission))
+                .orElse(false);
     }
 
     @Override
-    public boolean groupAdd(String worldName, String groupName, String permission) {
-        var holder = getWorldHolder(worldName);
-        var group = holder.getGroup(groupName);
-        if (group == null) return false;
-        group.addPermission(permission);
-        return true;
+    public boolean groupHas(@Nullable String world, @NotNull String groupName, @NotNull String permission) {
+        return getWorldHolder(null, world)
+                .map(holder -> holder.getGroup(groupName))
+                .map(group -> group.hasSamePermissionNode(permission))
+                .orElse(false);
     }
 
     @Override
-    public boolean groupRemove(String worldName, String groupName, String permission) {
-        var holder = getWorldHolder(worldName);
-        var group = holder.getGroup(groupName);
-        if (group == null) return false;
-        group.removePermission(permission);
-        return true;
+    public boolean groupAdd(@Nullable String world, @NotNull String groupName, @NotNull String permission) {
+        return getWorldHolder(null, world)
+                .map(holder -> holder.getGroup(groupName))
+                .map(group -> {
+                    group.addPermission(permission);
+                    return true;
+                }).orElse(false);
     }
 
     @Override
-    public boolean playerInGroup(String worldName, String playerName, String groupName) {
-        AnjoPermissionsHandler awh;
-        if (worldName == null) awh = getGroupManager().getWorldsHolder().getWorldPermissionsByPlayerName(playerName);
-        else awh = getGroupManager().getWorldsHolder().getWorldPermissions(worldName);
-        return awh != null && awh.inGroup(playerName, groupName);
+    public boolean groupRemove(@Nullable String worldName, @NotNull String groupName, @NotNull String permission) {
+        return getWorldHolder(null, worldName)
+                .map(holder -> holder.getGroup(groupName))
+                .map(group -> group.removePermission(permission))
+                .orElse(false);
     }
 
     @Override
-    public boolean playerAddGroup(String worldName, String playerName, String groupName) {
-        if (playerName == null || groupName == null) return false;
-        OverloadedWorldHolder owh;
-        if (worldName == null) owh = getGroupManager().getWorldsHolder().getWorldDataByPlayerName(playerName);
-        else owh = getGroupManager().getWorldsHolder().getWorldData(worldName);
-        if (owh == null) return false;
-        var user = owh.getUser(playerName);
-        if (user == null) return false;
-        var group = owh.getGroup(groupName);
-        if (group == null) return false;
-        if (user.getGroup().equals(owh.getDefaultGroup())) user.setGroup(group);
-        else if (group.getInherits().contains(user.getGroup().getName().toLowerCase())) user.setGroup(group);
-        else user.addSubGroup(group);
-        var player = Bukkit.getPlayer(playerName);
-        if (player != null) GroupManager.getBukkitPermissions().updatePermissions(player);
-        return true;
+    public boolean playerInGroup(@Nullable String worldName, @Nullable String name, @NotNull String groupName) {
+        return getWorldHolder(name, worldName)
+                .map(WorldDataHolder::getPermissionsHandler)
+                .map(handler -> handler.inGroup(name, groupName))
+                .orElse(false);
     }
 
     @Override
-    public boolean playerRemoveGroup(String worldName, String playerName, String groupName) {
-        if (playerName == null || groupName == null) return false;
-        var worldHolder = getWorldHolder(worldName);
-        var user = worldHolder.getUser(playerName);
-        if (user == null) return false;
-        var success = false;
-        if (user.getGroup().getName().equalsIgnoreCase(groupName)) {
-            user.setGroup(worldHolder.getDefaultGroup());
-            success = true;
-        } else {
-            var group = worldHolder.getGroup(groupName);
-            if (group != null) success = user.removeSubGroup(group);
-        }
-        if (success) {
-            var player = Bukkit.getPlayer(playerName);
-            if (player != null) GroupManager.getBukkitPermissions().updatePermissions(player);
-        }
-        return success;
+    public boolean playerAddGroup(@Nullable String world, @Nullable String name, @NotNull String groupName) {
+        return getWorldHolder(name, world).map(holder -> {
+            var user = holder.getUser(name);
+            var group = holder.getGroup(groupName);
+            if (user == null || group == null) return false;
+            if (user.getGroup().equals(holder.getDefaultGroup())) user.setGroup(group);
+            else if (group.getInherits().contains(user.getGroup().getName().toLowerCase())) user.setGroup(group);
+            else return user.addSubGroup(group);
+            return true;
+        }).orElse(false);
     }
 
     @Override
-    public String[] getPlayerGroups(String worldName, String playerName) {
-        if (playerName == null) return new String[0];
-        AnjoPermissionsHandler handler;
-        if (worldName == null) {
-            handler = getGroupManager().getWorldsHolder().getWorldPermissionsByPlayerName(playerName);
-        } else {
-            handler = getGroupManager().getWorldsHolder().getWorldPermissions(worldName);
-        }
-        return handler != null ? handler.getGroups(playerName) : new String[0];
+    public boolean playerRemoveGroup(@Nullable String world, @Nullable String name, @NotNull String groupName) {
+        return getWorldHolder(name, world).map(holder -> {
+            var user = holder.getUser(name);
+            var group = holder.getGroup(groupName);
+            if (user == null || group == null) return false;
+            if (user.getGroup().equals(group)) user.setGroup(holder.getDefaultGroup());
+            else return user.removeSubGroup(group);
+            return true;
+        }).orElse(false);
     }
 
     @Override
-    public @Nullable String getPrimaryGroup(String worldName, String playerName) {
-        if (playerName == null) return null;
-        AnjoPermissionsHandler handler;
-        if (worldName == null) {
-            handler = getGroupManager().getWorldsHolder().getWorldPermissionsByPlayerName(playerName);
-        } else {
-            handler = getGroupManager().getWorldsHolder().getWorldPermissions(worldName);
-        }
-        if (handler == null) return null;
-        return handler.getGroup(playerName);
+    public @NotNull String[] getPlayerGroups(@Nullable String world, @Nullable String name) {
+        return getWorldHolder(name, world)
+                .map(WorldDataHolder::getPermissionsHandler)
+                .map(handler -> handler.getGroups(name))
+                .orElseGet(() -> new String[]{});
     }
 
     @Override
-    public String[] getGroups() {
+    public @Nullable String getPrimaryGroup(@Nullable String world, @Nullable String name) {
+        return getWorldHolder(name, world)
+                .map(WorldDataHolder::getPermissionsHandler)
+                .map(handler -> handler.getGroup(name))
+                .orElse(null);
+    }
+
+    @Override
+    public @NotNull String[] getGroups() {
         return plugin.getServer().getWorlds().stream()
                 .map(WorldInfo::getName)
                 .map(getGroupManager().getWorldsHolder()::getWorldData)
-                .filter(Objects::nonNull)
                 .map(OverloadedWorldHolder::getGroupList)
-                .filter(Objects::nonNull)
                 .<String>mapMulti((groups, consumer) -> groups.stream()
                         .map(Group::getName).forEach(consumer))
                 .toList().toArray(new String[]{});
