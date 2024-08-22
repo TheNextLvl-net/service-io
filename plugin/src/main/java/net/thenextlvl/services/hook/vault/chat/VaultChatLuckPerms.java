cfa9ed52789cac27c0c5f73593a3e51f8b9b1823
david <mrminecraft00@gmail.com>
2024-08-22 18:10:31 +0200

    Refactor VaultChatLuckPerms for better null handling
    This commit improves the code by adding better null handling and utilizing Optional to avoid potential null pointer exceptions. It also adds annotations for better code readability and safety. The refactoring enhances the methods for managing prefixes, suffixes, and meta values for both users and groups.

--- plugin/src/main/java/net/thenextlvl/services/hook/vault/chat/VaultChatLuckPerms.java
+++ plugin/src/main/java/net/thenextlvl/services/hook/vault/chat/VaultChatLuckPerms.java
@@ -3,160 +3,225 @@ package net.thenextlvl.services.hook.vault.chat;
 import lombok.Getter;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.cacheddata.CachedDataManager;
+import net.luckperms.api.cacheddata.CachedMetaData;
 import net.luckperms.api.context.ImmutableContextSet;
 import net.luckperms.api.model.PermissionHolder;
-import net.luckperms.api.query.QueryMode;
+import net.luckperms.api.model.group.Group;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.node.types.MetaNode;
+import net.luckperms.api.node.types.PrefixNode;
+import net.luckperms.api.node.types.SuffixNode;
 import net.luckperms.api.query.QueryOptions;
 import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.permission.Permission;
 import net.thenextlvl.services.ServicePlugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 
 @Getter
 public class VaultChatLuckPerms extends Chat {
-    private final String name = "LuckPerms - Chat";
-    private final LuckPerms luckPerms;
+    private final @NotNull String name = "LuckPerms - Chat";
+    private final @NotNull LuckPerms luckPerms;
 
-    public VaultChatLuckPerms(ServicePlugin plugin, Permission permission) {
+    public VaultChatLuckPerms(@NotNull ServicePlugin plugin, @NotNull Permission permission) {
         super(permission);
         this.luckPerms = LuckPermsProvider.get();
     }
 
+    private @NotNull Optional<User> getUser(@Nullable String name) {
+        return Optional.ofNullable(name).map(getLuckPerms().getUserManager()::getUser);
+    }
+
+    private @NotNull Optional<Group> getGroup(@Nullable String name) {
+        return Optional.ofNullable(name).map(getLuckPerms().getGroupManager()::getGroup);
+    }
+
+    private @NotNull Optional<CachedMetaData> getMetaData(@NotNull CachedDataManager manager, @Nullable String world) {
+        var options = Optional.ofNullable(world).map(s -> QueryOptions.contextual(ImmutableContextSet.of("world", s)));
+        return options.map(manager::getMetaData).or(() -> Optional.of(manager.getMetaData()));
+    }
+
     @Override
-    public String getPlayerPrefix(String world, String player) {
-        if (world == null || player == null) return "";
-        var user = luckPerms.getUserManager().getUser(player);
-        if (user == null) return "";
-        var options = QueryOptions.builder(QueryMode.CONTEXTUAL)
-                .context(ImmutableContextSet.of("world", world))
-                .build();
-        return Optional.of(user).map(PermissionHolder::getCachedData)
-                .map(data -> data.getMetaData(options).getPrefix())
+    public @NotNull String getPlayerPrefix(@Nullable String world, @Nullable String player) {
+        return getUser(player).map(PermissionHolder::getCachedData)
+                .flatMap(manager -> getMetaData(manager, world))
+                .map(CachedMetaData::getPrefix)
                 .orElse("");
     }
 
     @Override
-    public void setPlayerPrefix(String world, String player, String prefix) {
-
+    public void setPlayerPrefix(@Nullable String world, @Nullable String player, @NotNull String prefix) {
+        getUser(player).ifPresent(user -> {
+            var data = user.data();
+            data.add(PrefixNode.builder().prefix(prefix).build());
+            getLuckPerms().getUserManager().saveUser(user);
+        });
     }
 
     @Override
-    public String getPlayerSuffix(String world, String player) {
-        if (world == null || player == null) return "";
-        var user = luckPerms.getUserManager().getUser(player);
-        if (user == null) return "";
-        var options = QueryOptions.builder(QueryMode.CONTEXTUAL)
-                .context(ImmutableContextSet.of("world", world))
-                .build();
-        return Optional.of(user).map(PermissionHolder::getCachedData)
-                .map(data -> data.getMetaData(options).getSuffix())
+    public @NotNull String getPlayerSuffix(@Nullable String world, @Nullable String player) {
+        return getUser(player).map(PermissionHolder::getCachedData)
+                .flatMap(manager -> getMetaData(manager, world))
+                .map(CachedMetaData::getSuffix)
                 .orElse("");
     }
 
     @Override
-    public void setPlayerSuffix(String world, String player, String suffix) {
-
+    public void setPlayerSuffix(@Nullable String world, @Nullable String player, @NotNull String suffix) {
+        getUser(player).ifPresent(user -> {
+            var data = user.data();
+            data.add(SuffixNode.builder().suffix(suffix).build());
+            getLuckPerms().getUserManager().saveUser(user);
+        });
     }
 
     @Override
-    public String getGroupPrefix(String world, String group) {
-        return "";
+    public @NotNull String getGroupPrefix(@Nullable String world, @Nullable String group) {
+        return getGroup(group).map(PermissionHolder::getCachedData)
+                .flatMap(manager -> getMetaData(manager, world))
+                .map(CachedMetaData::getPrefix)
+                .orElse("");
     }
 
     @Override
-    public void setGroupPrefix(String world, String group, String prefix) {
-
+    public void setGroupPrefix(@Nullable String world, @Nullable String name, @NotNull String prefix) {
+        getGroup(name).ifPresent(group -> {
+            var data = group.data();
+            data.add(PrefixNode.builder().prefix(prefix).build());
+            getLuckPerms().getGroupManager().saveGroup(group);
+        });
     }
 
     @Override
-    public String getGroupSuffix(String world, String group) {
-        return "";
+    public @NotNull String getGroupSuffix(@Nullable String world, @Nullable String group) {
+        return getGroup(group).map(PermissionHolder::getCachedData)
+                .flatMap(manager -> getMetaData(manager, world))
+                .map(CachedMetaData::getSuffix)
+                .orElse("");
     }
 
     @Override
-    public void setGroupSuffix(String world, String group, String suffix) {
-
+    public void setGroupSuffix(@Nullable String world, @Nullable String name, @NotNull String suffix) {
+        getGroup(name).ifPresent(group -> {
+            var data = group.data();
+            data.add(SuffixNode.builder().suffix(suffix).build());
+            getLuckPerms().getGroupManager().saveGroup(group);
+        });
     }
 
     @Override
-    public int getPlayerInfoInteger(String world, String player, String node, int defaultValue) {
-        return 0;
+    public int getPlayerInfoInteger(@Nullable String world, @Nullable String player, @NotNull String node, int defaultValue) {
+        return getUser(player).map(PermissionHolder::getCachedData)
+                .flatMap(manager -> getMetaData(manager, world))
+                .flatMap(data -> data.getMetaValue(node, Integer::parseInt))
+                .orElse(defaultValue);
     }
 
     @Override
-    public void setPlayerInfoInteger(String world, String player, String node, int value) {
-
+    public void setPlayerInfoInteger(@Nullable String world, @Nullable String player, @NotNull String node, int value) {
+        setPlayerInfoString(world, player, node, String.valueOf(value));
     }
 
     @Override
-    public int getGroupInfoInteger(String world, String group, String node, int defaultValue) {
-        return 0;
+    public int getGroupInfoInteger(@Nullable String world, @Nullable String group, @NotNull String node, int defaultValue) {
+        return getGroup(group).map(PermissionHolder::getCachedData)
+                .flatMap(manager -> getMetaData(manager, world))
+                .flatMap(data -> data.getMetaValue(node, Integer::parseInt))
+                .orElse(defaultValue);
     }
 
     @Override
-    public void setGroupInfoInteger(String world, String group, String node, int value) {
-
+    public void setGroupInfoInteger(@Nullable String world, @Nullable String name, @NotNull String node, int value) {
+        setGroupInfoString(world, name, node, String.valueOf(value));
     }
 
     @Override
-    public double getPlayerInfoDouble(String world, String player, String node, double defaultValue) {
-        return 0;
+    public double getPlayerInfoDouble(@Nullable String world, @Nullable String player, @NotNull String node, double defaultValue) {
+        return getUser(player).map(PermissionHolder::getCachedData)
+                .flatMap(manager -> getMetaData(manager, world))
+                .flatMap(data -> data.getMetaValue(node, Double::parseDouble))
+                .orElse(defaultValue);
     }
 
     @Override
-    public void setPlayerInfoDouble(String world, String player, String node, double value) {
-
+    public void setPlayerInfoDouble(@Nullable String world, @Nullable String player, @NotNull String node, double value) {
+        setPlayerInfoString(world, player, node, String.valueOf(value));
     }
 
     @Override
-    public double getGroupInfoDouble(String world, String group, String node, double defaultValue) {
-        return 0;
+    public double getGroupInfoDouble(@Nullable String world, @Nullable String group, @NotNull String node, double defaultValue) {
+        return getGroup(group).map(PermissionHolder::getCachedData)
+                .flatMap(manager -> getMetaData(manager, world))
+                .flatMap(data -> data.getMetaValue(node, Double::parseDouble))
+                .orElse(defaultValue);
     }
 
     @Override
-    public void setGroupInfoDouble(String world, String group, String node, double value) {
-
+    public void setGroupInfoDouble(@Nullable String world, @Nullable String group, @NotNull String node, double value) {
+        setGroupInfoString(world, group, node, String.valueOf(value));
     }
 
     @Override
-    public boolean getPlayerInfoBoolean(String world, String player, String node, boolean defaultValue) {
-        return false;
+    public boolean getPlayerInfoBoolean(@Nullable String world, @Nullable String player, @NotNull String node, boolean defaultValue) {
+        return getUser(player).map(PermissionHolder::getCachedData)
+                .flatMap(manager -> getMetaData(manager, world))
+                .flatMap(data -> data.getMetaValue(node, Boolean::parseBoolean))
+                .orElse(defaultValue);
     }
 
     @Override
-    public void setPlayerInfoBoolean(String world, String player, String node, boolean value) {
-
+    public void setPlayerInfoBoolean(@Nullable String world, @Nullable String player, @NotNull String node, boolean value) {
+        setPlayerInfoString(world, player, node, String.valueOf(value));
     }
 
     @Override
-    public boolean getGroupInfoBoolean(String world, String group, String node, boolean defaultValue) {
-        return false;
+    public boolean getGroupInfoBoolean(@Nullable String world, @Nullable String group, @NotNull String node, boolean defaultValue) {
+        return getGroup(group).map(PermissionHolder::getCachedData)
+                .flatMap(manager -> getMetaData(manager, world))
+                .flatMap(data -> data.getMetaValue(node, Boolean::parseBoolean))
+                .orElse(defaultValue);
     }
 
     @Override
-    public void setGroupInfoBoolean(String world, String group, String node, boolean value) {
-
+    public void setGroupInfoBoolean(@Nullable String world, @Nullable String group, @NotNull String node, boolean value) {
+        setGroupInfoString(world, group, node, String.valueOf(value));
     }
 
     @Override
-    public String getPlayerInfoString(String world, String player, String node, String defaultValue) {
-        return "";
+    public @Nullable String getPlayerInfoString(@Nullable String world, @Nullable String player, @NotNull String node, @Nullable String defaultValue) {
+        return getUser(player).map(PermissionHolder::getCachedData)
+                .flatMap(manager -> getMetaData(manager, world))
+                .map(data -> data.getMetaValue(node))
+                .orElse(defaultValue);
     }
 
     @Override
-    public void setPlayerInfoString(String world, String player, String node, String value) {
-
+    public void setPlayerInfoString(@Nullable String world, @Nullable String player, @NotNull String node, @NotNull String value) {
+        getUser(player).ifPresent(user -> {
+            var data = user.data();
+            data.add(MetaNode.builder(node, value).build());
+            getLuckPerms().getUserManager().saveUser(user);
+        });
     }
 
     @Override
-    public String getGroupInfoString(String world, String group, String node, String defaultValue) {
-        return "";
+    public @Nullable String getGroupInfoString(@Nullable String world, @Nullable String group, @NotNull String node, @Nullable String defaultValue) {
+        return getGroup(group).map(PermissionHolder::getCachedData)
+                .flatMap(manager -> getMetaData(manager, world))
+                .map(data -> data.getMetaValue(node))
+                .orElse(defaultValue);
     }
 
     @Override
-    public void setGroupInfoString(String world, String group, String node, String value) {
-
+    public void setGroupInfoString(@Nullable String world, @Nullable String name, @NotNull String node, @NotNull String value) {
+        getGroup(name).ifPresent(group -> {
+            var data = group.data();
+            data.add(MetaNode.builder(node, value).build());
+            getLuckPerms().getGroupManager().saveGroup(group);
+        });
     }
 
     @Override
