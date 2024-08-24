6c774d5fdcff33084d82f90bfb5e974d93bc3c40
david <mrminecraft00@gmail.com>
2024-08-24 16:37:07 +0200

    Update LuckPermsPermissionHolder with Group support.
    Added methods to manage user groups, including adding, removing, and checking group membership. Integrated `GroupHolder` interface to enhance permission holder functionality, allowing seamless group-related operations.

--- plugin/src/main/java/net/thenextlvl/service/model/permission/LuckPermsPermissionHolder.java
+++ plugin/src/main/java/net/thenextlvl/service/model/permission/LuckPermsPermissionHolder.java
@@ -3,11 +3,19 @@ package net.thenextlvl.service.model.permission;
 import net.kyori.adventure.util.TriState;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.user.User;
+import net.luckperms.api.node.types.MetaNode;
 import net.luckperms.api.node.types.PermissionNode;
 import net.luckperms.api.query.QueryOptions;
-import net.thenextlvl.service.api.permission.PermissionHolder;
+import net.thenextlvl.service.api.group.Group;
+import net.thenextlvl.service.api.group.GroupHolder;
+import net.thenextlvl.service.model.group.LuckPermsGroup;
 
-public record LuckPermsPermissionHolder(User user, QueryOptions options) implements PermissionHolder {
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public record LuckPermsPermissionHolder(User user, QueryOptions options) implements GroupHolder {
     @Override
     public TriState checkPermission(String permission) {
         return switch (user().getCachedData().getPermissionData(options()).checkPermission(permission)) {
@@ -29,5 +37,83 @@ public record LuckPermsPermissionHolder(User user, QueryOptions options) impleme
         var result = user().data().add(PermissionNode.builder(permission).context(options().context()).build());
         LuckPermsProvider.get().getUserManager().saveUser(user());
         return result.wasSuccessful();
+    }
+
+    @Override
+    public <T> Optional<T> getInfoNode(String key, Function<String, T> mapper) {
+        return user().getCachedData().getMetaData(options()).getMetaValue(key, mapper);
+    }
+
+    @Override
+    public boolean setInfoNode(String key, String value) {
+        var result = user().data().add(MetaNode.builder(key, value).context(options().context()).build());
+        LuckPermsProvider.get().getUserManager().saveUser(user());
+        return result.wasSuccessful();
+    }
+
+    @Override
+    public boolean removeInfoNode(String key) {
+        user().data().clear(options().context(), node -> node.getKey().equals(key));
+        LuckPermsProvider.get().getUserManager().saveUser(user());
+        return true;
+    }
+
+    @Override
+    public boolean removeInfoNode(String key, String value) {
+        var result = user().data().remove(MetaNode.builder(key, value).context(options().context()).build());
+        LuckPermsProvider.get().getUserManager().saveUser(user());
+        return result.wasSuccessful();
+    }
+
+    @Override
+    public Set<Group> getGroups() {
+        return user().getInheritedGroups(options()).stream()
+                .map(group -> new LuckPermsGroup(group, options()))
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    @Override
+    public String getPrimaryGroup() {
+        return user().getPrimaryGroup();
+    }
+
+    @Override
+    public boolean addGroup(Group group) {
+        return addGroup(group.getName());
+    }
+
+    @Override
+    public boolean addGroup(String group) {
+        return addPermission("group." + group);
+    }
+
+    @Override
+    public boolean inGroup(Group group) {
+        return inGroup(group.getName());
+    }
+
+    @Override
+    public boolean inGroup(String group) {
+        return checkPermission("group." + group).equals(TriState.TRUE);
+    }
+
+    @Override
+    public boolean removeGroup(Group group) {
+        return removeGroup(group.getName());
+    }
+
+    @Override
+    public boolean removeGroup(String group) {
+        return removePermission("group." + group);
+    }
+
+    @Override
+    public boolean setPrimaryGroup(Group group) {
+        return setPrimaryGroup(group.getName());
+    }
+
+    @Override
+    public boolean setPrimaryGroup(String group) {
+        return user.setPrimaryGroup(group).wasSuccessful();
     }
 }
