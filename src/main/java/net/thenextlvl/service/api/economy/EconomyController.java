22b1a0b317366cb781822b712a1e3099bb049233
david <mrminecraft00@gmail.com>
2024-08-24 16:38:45 +0200

    Add new currency methods and update existing UUID methods
    Introduce methods for currency name, symbol, and formatting to `EconomyController`. Also, revised method parameters from `uniqueId` to `uuid` and synchronized return types for delete operations to `CompletableFuture<Boolean>`.

--- src/main/java/net/thenextlvl/service/api/economy/EconomyController.java
+++ src/main/java/net/thenextlvl/service/api/economy/EconomyController.java
@@ -3,6 +3,7 @@ package net.thenextlvl.service.api.economy;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
 
+import java.util.Locale;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
@@ -10,6 +11,44 @@ import java.util.concurrent.CompletableFuture;
  * The AccountController interface provides methods to create, retrieve and delete accounts.
  */
 public interface EconomyController {
+    /**
+     * Retrieves the name associated with the economy controller.
+     *
+     * @return the name of the economy controller.
+     */
+    String getName();
+
+    /**
+     * Formats the specified amount as a string.
+     *
+     * @param amount the number amount to be formatted
+     * @return the formatted amount as a string
+     */
+    String format(Number amount);
+
+    /**
+     * Retrieves the plural form of the currency name based on the provided locale.
+     *
+     * @param locale the locale for which to retrieve the plural currency name
+     * @return the plural form of the currency name as a string
+     */
+    String getCurrencyNamePlural(Locale locale);
+
+    /**
+     * Retrieves the name of the currency associated with the specified locale.
+     *
+     * @param locale the locale for which to retrieve the currency name
+     * @return the name of the currency as a string
+     */
+    String getCurrencyNameSingular(Locale locale);
+
+    /**
+     * Retrieves the currency symbol associated with the economy controller.
+     *
+     * @return the currency symbol as a string
+     */
+    String getCurrencySymbol();
+
     /**
      * Creates an account for the specified player.
      *
@@ -30,19 +69,19 @@ public interface EconomyController {
     /**
      * Creates an account with the given unique ID.
      *
-     * @param uniqueId the unique ID of the account to be created
+     * @param uuid the unique ID of the account to be created
      * @return a CompletableFuture that will complete with the created account
      */
-    CompletableFuture<Account> createAccount(UUID uniqueId);
+    CompletableFuture<Account> createAccount(UUID uuid);
 
     /**
      * Creates an account with the given unique ID and world.
      *
-     * @param uniqueId the unique ID of the account to be created
-     * @param world    the world in which the account will be created
+     * @param uuid  the unique ID of the account to be created
+     * @param world the world in which the account will be created
      * @return a CompletableFuture that will complete with the created account
      */
-    CompletableFuture<Account> createAccount(UUID uniqueId, World world);
+    CompletableFuture<Account> createAccount(UUID uuid, World world);
 
     /**
      * Retrieves the account for the specified player.
@@ -53,21 +92,30 @@ public interface EconomyController {
     CompletableFuture<Account> getAccount(OfflinePlayer player);
 
     /**
-     * Retrieves the account with the specified unique ID.
+     * Retrieves the account for the specified unique ID and world.
      *
-     * @param uniqueId the unique ID of the account to be retrieved
+     * @param player the player for whom the account will be retrieved
+     * @param world  the world in which the account is located
      * @return a CompletableFuture that will complete with the retrieved account
      */
-    CompletableFuture<Account> getAccount(UUID uniqueId);
+    CompletableFuture<Account> getAccount(OfflinePlayer player, World world);
+
+    /**
+     * Retrieves the account with the specified unique ID.
+     *
+     * @param uuid the unique ID of the account to be retrieved
+     * @return a CompletableFuture that will complete with the retrieved account
+     */
+    CompletableFuture<Account> getAccount(UUID uuid);
 
     /**
      * Retrieves the account for the specified unique ID and world.
      *
-     * @param uniqueId the unique ID of the account to be retrieved
-     * @param world    the world in which the account is located
+     * @param uuid  the unique ID of the account to be retrieved
+     * @param world the world in which the account is located
      * @return a CompletableFuture that will complete with the retrieved account
      */
-    CompletableFuture<Account> getAccount(UUID uniqueId, World world);
+    CompletableFuture<Account> getAccount(UUID uuid, World world);
 
     /**
      * Deletes the specified account.
@@ -75,7 +123,7 @@ public interface EconomyController {
      * @param account the account to be deleted
      * @return a CompletableFuture that will complete when the account is deleted
      */
-    CompletableFuture<Void> deleteAccount(Account account);
+    CompletableFuture<Boolean> deleteAccount(Account account);
 
     /**
      * Deletes the account of the specified player.
@@ -83,7 +131,7 @@ public interface EconomyController {
      * @param player the player whose account will be deleted
      * @return a CompletableFuture that will complete when the account is deleted
      */
-    CompletableFuture<Void> deleteAccount(OfflinePlayer player);
+    CompletableFuture<Boolean> deleteAccount(OfflinePlayer player);
 
     /**
      * Deletes the account of the specified player in the specified world.
@@ -92,22 +140,29 @@ public interface EconomyController {
      * @param world  the world in which the player's account exists
      * @return a CompletableFuture that will complete when the account is deleted
      */
-    CompletableFuture<Void> deleteAccount(OfflinePlayer player, World world);
+    CompletableFuture<Boolean> deleteAccount(OfflinePlayer player, World world);
 
     /**
      * Deletes the account with the specified unique ID.
      *
-     * @param uniqueId the unique ID of the account to be deleted
+     * @param uuid the unique ID of the account to be deleted
      * @return a CompletableFuture that will complete when the account is deleted
      */
-    CompletableFuture<Void> deleteAccount(UUID uniqueId);
+    CompletableFuture<Boolean> deleteAccount(UUID uuid);
 
     /**
      * Deletes the account with the specified unique ID in the specified world.
      *
-     * @param uniqueId the unique ID of the account to be deleted
-     * @param world    the world in which the account exists
+     * @param uuid  the unique ID of the account to be deleted
+     * @param world the world in which the account exists
      * @return a CompletableFuture that will complete when the account is deleted
      */
-    CompletableFuture<Void> deleteAccount(UUID uniqueId, World world);
+    CompletableFuture<Boolean> deleteAccount(UUID uuid, World world);
+
+    /**
+     * Retrieves the number of fractional digits used for formatting currency amounts.
+     *
+     * @return the number of fractional digits used for formatting currency amounts
+     */
+    int fractionalDigits();
 }
