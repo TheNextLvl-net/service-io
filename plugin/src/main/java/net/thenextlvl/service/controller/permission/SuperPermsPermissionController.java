bde69434ba2177c6076215ea4de29aa6023a1bb6
david <mrminecraft00@gmail.com>
2024-08-25 09:11:07 +0200

    Add ServicePlugin dependency to SuperPermsPermissionController
    Introduced a required `ServicePlugin` dependency to access server methods, replacing direct `Bukkit` calls. This improves the flexibility and testability of the permission controller.

--- plugin/src/main/java/net/thenextlvl/service/controller/permission/SuperPermsPermissionController.java
+++ plugin/src/main/java/net/thenextlvl/service/controller/permission/SuperPermsPermissionController.java
@@ -1,10 +1,11 @@
 package net.thenextlvl.service.controller.permission;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import net.thenextlvl.service.ServicePlugin;
 import net.thenextlvl.service.api.permission.PermissionController;
 import net.thenextlvl.service.api.permission.PermissionHolder;
 import net.thenextlvl.service.model.permission.SuperPermsPermissionHolder;
-import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
@@ -13,9 +14,10 @@ import org.jetbrains.annotations.Nullable;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
-@Getter
+@RequiredArgsConstructor
 public class SuperPermsPermissionController implements PermissionController {
-    private final String name = "SuperPerms";
+    private final @Getter String name = "SuperPerms";
+    private final ServicePlugin plugin;
 
     @Override
     public CompletableFuture<PermissionHolder> getPermissionHolder(OfflinePlayer player) {
@@ -29,12 +31,12 @@ public class SuperPermsPermissionController implements PermissionController {
 
     @Override
     public CompletableFuture<PermissionHolder> getPermissionHolder(UUID uuid) {
-        return getPermissionHolder(Bukkit.getPlayer(uuid));
+        return getPermissionHolder(plugin.getServer().getPlayer(uuid));
     }
 
     @Override
     public CompletableFuture<PermissionHolder> getPermissionHolder(UUID uuid, World world) {
-        return getPermissionHolder(Bukkit.getPlayer(uuid));
+        return getPermissionHolder(plugin.getServer().getPlayer(uuid));
     }
 
     private CompletableFuture<PermissionHolder> getPermissionHolder(@Nullable Player player) {
