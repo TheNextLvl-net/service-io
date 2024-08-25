679c7855e54ffa00b8f8c9adf6af09972a66540a
david <mrminecraft00@gmail.com>
2024-08-25 09:08:19 +0200

    Rename methods and add synchronous getProfile
    Renamed async methods from getProfile to loadProfile for clarity. Added synchronous getProfile methods to retrieve user profiles without requiring futures, enhancing flexibility and performance.

--- plugin/src/main/java/net/thenextlvl/service/controller/chat/LuckPermsChatController.java
+++ plugin/src/main/java/net/thenextlvl/service/controller/chat/LuckPermsChatController.java
@@ -10,6 +10,7 @@ import net.thenextlvl.service.api.chat.ChatProfile;
 import net.thenextlvl.service.model.chat.LuckPermsChatProfile;
 import org.bukkit.World;
 
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
@@ -18,14 +19,28 @@ public class LuckPermsChatController implements ChatController {
     private final LuckPerms luckPerms = LuckPermsProvider.get();
 
     @Override
-    public CompletableFuture<ChatProfile> getProfile(UUID uuid) {
+    public CompletableFuture<ChatProfile> loadProfile(UUID uuid) {
         return luckPerms.getUserManager().loadUser(uuid).thenApply(user ->
                 new LuckPermsChatProfile(user, QueryOptions.defaultContextualOptions()));
     }
 
     @Override
-    public CompletableFuture<ChatProfile> getProfile(UUID uuid, World world) {
+    public CompletableFuture<ChatProfile> loadProfile(UUID uuid, World world) {
         return luckPerms.getUserManager().loadUser(uuid).thenApply(user -> {
+            var options = QueryOptions.contextual(ImmutableContextSet.of("world", world.getName()));
+            return new LuckPermsChatProfile(user, options);
+        });
+    }
+
+    @Override
+    public Optional<ChatProfile> getProfile(UUID uuid) {
+        return Optional.ofNullable(luckPerms.getUserManager().getUser(uuid)).map(user ->
+                new LuckPermsChatProfile(user, QueryOptions.defaultContextualOptions()));
+    }
+
+    @Override
+    public Optional<ChatProfile> getProfile(UUID uuid, World world) {
+        return Optional.ofNullable(luckPerms.getUserManager().getUser(uuid)).map(user -> {
             var options = QueryOptions.contextual(ImmutableContextSet.of("world", world.getName()));
             return new LuckPermsChatProfile(user, options);
         });
