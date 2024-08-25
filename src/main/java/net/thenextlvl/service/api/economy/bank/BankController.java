f324917c208006abaeb830112c44cafef407dce5
david <mrminecraft00@gmail.com>
2024-08-25 09:10:21 +0200

    Refactor BankController interface for enhanced functionality
    Added methods to create, load, and delete banks using both UUID and OfflinePlayer, with and without World parameters. Introduced optional methods to handle bank retrieval and deletion more efficiently, ensuring better support for future scalability.

--- src/main/java/net/thenextlvl/service/api/economy/bank/BankController.java
+++ src/main/java/net/thenextlvl/service/api/economy/bank/BankController.java
@@ -1,49 +1,107 @@
 package net.thenextlvl.service.api.economy.bank;
 
-import net.thenextlvl.service.api.economy.Account;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
+import org.jetbrains.annotations.Contract;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
-/**
- * The AccountController interface provides methods to create, retrieve and delete accounts.
- */
 public interface BankController {
-    CompletableFuture<Account> createBank(OfflinePlayer player);
+    default CompletableFuture<Bank> createBank(OfflinePlayer player, String name) throws IllegalStateException {
+        return createBank(player.getUniqueId(), name);
+    }
 
-    CompletableFuture<Account> createBank(OfflinePlayer player, World world);
+    default CompletableFuture<Bank> createBank(OfflinePlayer player, String name, World world) throws IllegalStateException {
+        return createBank(player.getUniqueId(), name, world);
+    }
 
-    CompletableFuture<Account> createBank(UUID uniqueId);
+    CompletableFuture<Bank> createBank(UUID uuid, String name) throws IllegalStateException;
 
-    CompletableFuture<Account> createBank(UUID uniqueId, World world);
+    CompletableFuture<Bank> createBank(UUID uuid, String name, World world) throws IllegalStateException;
 
-    CompletableFuture<Account> getAccount(OfflinePlayer player);
+    default CompletableFuture<Bank> loadBank(OfflinePlayer player) {
+        return loadBank(player.getUniqueId());
+    }
 
-    CompletableFuture<Account> getAccount(OfflinePlayer player, World world);
+    default CompletableFuture<Bank> loadBank(OfflinePlayer player, World world) {
+        return loadBank(player.getUniqueId(), world);
+    }
 
-    CompletableFuture<Account> getAccount(UUID uniqueId);
+    CompletableFuture<Bank> loadBank(UUID uuid);
 
-    CompletableFuture<Account> getAccount(UUID uniqueId, World world);
+    CompletableFuture<Bank> loadBank(UUID uuid, World world);
 
-    CompletableFuture<Boolean> deleteAccount(Account account);
+    CompletableFuture<Set<Bank>> loadBanks();
 
-    CompletableFuture<Boolean> deleteAccount(OfflinePlayer player);
+    CompletableFuture<Set<Bank>> loadBanks(String name);
 
-    CompletableFuture<Boolean> deleteAccount(OfflinePlayer player, World world);
+    CompletableFuture<Set<Bank>> loadBanks(String name, World world);
 
-    CompletableFuture<Boolean> deleteAccount(UUID uniqueId);
+    default CompletableFuture<Bank> tryGetBank(OfflinePlayer player) {
+        return getBank(player)
+                .map(CompletableFuture::completedFuture)
+                .orElseGet(() -> loadBank(player));
+    }
 
-    CompletableFuture<Boolean> deleteAccount(UUID uniqueId, World world);
+    default CompletableFuture<Bank> tryGetBank(OfflinePlayer player, World world) {
+        return getBank(player, world)
+                .map(CompletableFuture::completedFuture)
+                .orElseGet(() -> loadBank(player, world));
+    }
 
-    CompletableFuture<List<Bank>> getBanks();
+    default CompletableFuture<Bank> tryGetBank(UUID uuid) {
+        return getBank(uuid)
+                .map(CompletableFuture::completedFuture)
+                .orElseGet(() -> loadBank(uuid));
+    }
+
+    default CompletableFuture<Bank> tryGetBank(UUID uuid, World world) {
+        return getBank(uuid, world)
+                .map(CompletableFuture::completedFuture)
+                .orElseGet(() -> loadBank(uuid, world));
+    }
+
+    default CompletableFuture<Boolean> deleteBank(Bank bank) {
+        return bank.getWorld()
+                .map(world -> deleteBank(bank.getOwner(), world))
+                .orElseGet(() -> deleteBank(bank.getOwner()));
+    }
+
+    default CompletableFuture<Boolean> deleteBank(OfflinePlayer player) {
+        return deleteBank(player.getUniqueId());
+    }
+
+    default CompletableFuture<Boolean> deleteBank(OfflinePlayer player, World world) {
+        return deleteBank(player.getUniqueId(), world);
+    }
+
+    CompletableFuture<Boolean> deleteBank(UUID uuid);
+
+    CompletableFuture<Boolean> deleteBank(UUID uuid, World world);
+
+    List<Bank> getBanks();
+
+    default Optional<Bank> getBank(OfflinePlayer player) {
+        return getBank(player.getUniqueId());
+    }
+
+    default Optional<Bank> getBank(OfflinePlayer player, World world) {
+        return getBank(player.getUniqueId(), world);
+    }
+
+    Optional<Bank> getBank(UUID uuid);
+
+    Optional<Bank> getBank(UUID uuid, World world);
 
     /**
-     * Retrieves the name associated with the economy controller.
+     * Retrieves the name associated with the bank controller.
      *
-     * @return the name of the economy controller.
+     * @return the name of the bank controller.
      */
+    @Contract(pure = true)
     String getName();
 }
