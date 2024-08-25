32027067bcfcc115c296c2504afde4bd03c8fb0e
david <mrminecraft00@gmail.com>
2024-08-25 09:08:10 +0200

    Refactor profile retrieval to use Optional.
    Replaced CompletableFuture with Optional in profile retrieval methods. Added new methods to return CompletableFuture based on Optional values for better handling of null scenarios.

--- plugin/src/main/java/net/thenextlvl/service/controller/chat/GroupManagerChatController.java
+++ plugin/src/main/java/net/thenextlvl/service/controller/chat/GroupManagerChatController.java
@@ -11,6 +11,7 @@ import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
@@ -19,33 +20,60 @@ public class GroupManagerChatController implements ChatController {
     private final @Getter String name = "GroupManager";
 
     @Override
-    public CompletableFuture<ChatProfile> getProfile(OfflinePlayer player) {
+    public CompletableFuture<ChatProfile> loadProfile(OfflinePlayer player) {
+        return getProfile(player)
+                .map(CompletableFuture::completedFuture)
+                .orElseGet(() -> CompletableFuture.completedFuture(null));
+    }
+
+    @Override
+    public CompletableFuture<ChatProfile> loadProfile(OfflinePlayer player, World world) {
+        return getProfile(player, world)
+                .map(CompletableFuture::completedFuture)
+                .orElseGet(() -> CompletableFuture.completedFuture(null));
+    }
+
+    @Override
+    public CompletableFuture<ChatProfile> loadProfile(UUID uuid) {
+        return getProfile(uuid)
+                .map(CompletableFuture::completedFuture)
+                .orElseGet(() -> CompletableFuture.completedFuture(null));
+    }
+
+    @Override
+    public CompletableFuture<ChatProfile> loadProfile(UUID uuid, World world) {
+        return getProfile(uuid, world)
+                .map(CompletableFuture::completedFuture)
+                .orElseGet(() -> CompletableFuture.completedFuture(null));
+    }
+
+    @Override
+    public Optional<ChatProfile> getProfile(OfflinePlayer player) {
         var holder = groupManager.getWorldsHolder().getDefaultWorld();
         return getProfile(holder, player.getUniqueId(), player.getName());
     }
 
     @Override
-    public CompletableFuture<ChatProfile> getProfile(OfflinePlayer player, World world) {
+    public Optional<ChatProfile> getProfile(OfflinePlayer player, World world) {
         var holder = groupManager.getWorldsHolder().getWorldData(world.getName());
         return getProfile(holder, player.getUniqueId(), player.getName());
     }
 
     @Override
-    public CompletableFuture<ChatProfile> getProfile(UUID uuid) {
+    public Optional<ChatProfile> getProfile(UUID uuid) {
         var holder = groupManager.getWorldsHolder().getDefaultWorld();
         return getProfile(holder, uuid, null);
     }
 
     @Override
-    public CompletableFuture<ChatProfile> getProfile(UUID uuid, World world) {
+    public Optional<ChatProfile> getProfile(UUID uuid, World world) {
         var holder = groupManager.getWorldsHolder().getWorldData(world.getName());
         return getProfile(holder, uuid, null);
     }
 
-    private CompletableFuture<ChatProfile> getProfile(@Nullable WorldDataHolder holder, UUID uuid, @Nullable String name) {
-        if (holder == null) return CompletableFuture.completedFuture(null);
+    private Optional<ChatProfile> getProfile(@Nullable WorldDataHolder holder, UUID uuid, @Nullable String name) {
+        if (holder == null) return Optional.empty();
         var user = name != null ? holder.getUser(uuid.toString(), name) : holder.getUser(uuid.toString());
-        if (user == null) return CompletableFuture.completedFuture(null);
-        return CompletableFuture.completedFuture(new GroupManagerChatProfile(user, holder));
+        return user != null ? Optional.of(new GroupManagerChatProfile(user, holder)) : Optional.empty();
     }
 }
