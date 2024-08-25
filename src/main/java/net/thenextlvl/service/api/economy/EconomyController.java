72a0f51a130c7d810662a17f3172dcfd5ffc7ef4
david <mrminecraft00@gmail.com>
2024-08-25 09:07:23 +0200

    Add methods to retrieve, load, and delete accounts by UUID
    Introduced methods to handle accounts using UUIDs, providing options to retrieve, load asynchronously, and delete. These enhancements improve flexibility in account management and ensure compatibility with standard operations using UUIDs.

--- src/main/java/net/thenextlvl/service/api/economy/EconomyController.java
+++ src/main/java/net/thenextlvl/service/api/economy/EconomyController.java
@@ -2,8 +2,10 @@ package net.thenextlvl.service.api.economy;
 
 import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
+import org.jetbrains.annotations.Contract;
 
 import java.util.Locale;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
@@ -11,13 +13,6 @@ import java.util.concurrent.CompletableFuture;
  * The AccountController interface provides methods to create, retrieve and delete accounts.
  */
 public interface EconomyController {
-    /**
-     * Retrieves the name associated with the economy controller.
-     *
-     * @return the name of the economy controller.
-     */
-    String getName();
-
     /**
      * Formats the specified amount as a string.
      *
@@ -50,12 +45,103 @@ public interface EconomyController {
     String getCurrencySymbol();
 
     /**
+     * Retrieve the account for the specified player.
+     *
+     * @param player the player for whom the account will be retrieved
+     * @return an optional containing the account, or empty
+     */
+    default Optional<Account> getAccount(OfflinePlayer player) {
+        return getAccount(player.getUniqueId());
+    }
+
+    /**
+     * Retrieve the account for the specified uuid and world.
+     *
+     * @param player the player for whom the account will be retrieved
+     * @param world  the world in which the account is located
+     * @return an optional containing the account, or empty
+     */
+    default Optional<Account> getAccount(OfflinePlayer player, World world) {
+        return getAccount(player.getUniqueId(), world);
+    }
+
+    /**
+     * Retrieve the account with the specified uuid.
+     *
+     * @param uuid the uuid of the account to be retrieved
+     * @return an optional containing the account, or empty
+     */
+    Optional<Account> getAccount(UUID uuid);
+
+    /**
+     * Retrieve the account for the specified uuid and world.
+     *
+     * @param uuid  the uuid of the account to be retrieved
+     * @param world the world in which the account is located
+     * @return an optional containing the account, or empty
+     */
+    Optional<Account> getAccount(UUID uuid, World world);
+
+    /**
+     * Retrieve the account for the specified player or try to load it.
+     *
+     * @param player the player for whom the account will be retrieved
+     * @return a CompletableFuture that will complete with the retrieved account
+     */
+    default CompletableFuture<Account> tryGetAccount(OfflinePlayer player) {
+        return getAccount(player)
+                .map(CompletableFuture::completedFuture)
+                .orElseGet(() -> loadAccount(player));
+    }
+
+    /**
+     * Retrieve the account for the specified player and world or try to load it.
+     *
+     * @param player the player for whom the account will be retrieved
+     * @param world  the world in which the account is located
+     * @return a CompletableFuture that will complete with the retrieved account
+     */
+    default CompletableFuture<Account> tryGetAccount(OfflinePlayer player, World world) {
+        return getAccount(player, world)
+                .map(CompletableFuture::completedFuture)
+                .orElseGet(() -> loadAccount(player, world));
+    }
+
+    /**
+     * Retrieve the account for the specified uuid or try to load it.
+     *
+     * @param uuid the uuid of the account to be retrieved
+     * @return a CompletableFuture that will complete with the retrieved account
+     */
+    default CompletableFuture<Account> tryGetAccount(UUID uuid) {
+        return getAccount(uuid)
+                .map(CompletableFuture::completedFuture)
+                .orElseGet(() -> loadAccount(uuid));
+    }
+
+    /**
+     * Retrieve the account for the specified uuid and world or try to load it.
+     *
+     * @param uuid  the uuid of the account to be retrieved
+     * @param world the world in which the account is located
+     * @return a CompletableFuture that will complete with the retrieved account
+     */
+    default CompletableFuture<Account> tryGetAccount(UUID uuid, World world) {
+        return getAccount(uuid, world)
+                .map(CompletableFuture::completedFuture)
+                .orElseGet(() -> loadAccount(uuid, world));
+    }
+
+    /**
      * Creates an account for the specified player.
      *
      * @param player the player for whom the account will be created
      * @return a CompletableFuture that will complete with the created account
+     * @throws IllegalStateException if a similar account already exists
      */
-    CompletableFuture<Account> createAccount(OfflinePlayer player);
+    default CompletableFuture<Account> createAccount(OfflinePlayer player) throws IllegalStateException {
+        return createAccount(player.getUniqueId());
+    }
 
     /**
      * Creates an account for the specified player in the specified world.
@@ -63,59 +149,68 @@ public interface EconomyController {
      * @param player the player for whom the account will be created
      * @param world  the world in which the player's account will be created
      * @return a CompletableFuture that will complete with the created account
+     * @throws IllegalStateException if a similar account already exists
      */
-    CompletableFuture<Account> createAccount(OfflinePlayer player, World world);
+    default CompletableFuture<Account> createAccount(OfflinePlayer player, World world) throws IllegalStateException {
+        return createAccount(player.getUniqueId(), world);
+    }
 
     /**
-     * Creates an account with the given unique ID.
+     * Creates an account with the given uuid.
      *
-     * @param uuid the unique ID of the account to be created
+     * @param uuid the uuid of the account to be created
      * @return a CompletableFuture that will complete with the created account
+     * @throws IllegalStateException if a similar account already exists
      */
-    CompletableFuture<Account> createAccount(UUID uuid);
+    CompletableFuture<Account> createAccount(UUID uuid) throws IllegalStateException;
 
     /**
-     * Creates an account with the given unique ID and world.
+     * Creates an account with the given uuid and world.
      *
-     * @param uuid  the unique ID of the account to be created
+     * @param uuid  the uuid of the account to be created
      * @param world the world in which the account will be created
      * @return a CompletableFuture that will complete with the created account
+     * @throws IllegalStateException if a similar account already exists
      */
-    CompletableFuture<Account> createAccount(UUID uuid, World world);
+    CompletableFuture<Account> createAccount(UUID uuid, World world) throws IllegalStateException;
 
     /**
-     * Retrieves the account for the specified player.
+     * Loads the account for the specified player asynchronously.
      *
      * @param player the player for whom the account will be retrieved
      * @return a CompletableFuture that will complete with the retrieved account
      */
-    CompletableFuture<Account> getAccount(OfflinePlayer player);
+    default CompletableFuture<Account> loadAccount(OfflinePlayer player) {
+        return loadAccount(player.getUniqueId());
+    }
 
     /**
-     * Retrieves the account for the specified unique ID and world.
+     * Loads the account for the specified uuid and world asynchronously.
      *
      * @param player the player for whom the account will be retrieved
      * @param world  the world in which the account is located
      * @return a CompletableFuture that will complete with the retrieved account
      */
-    CompletableFuture<Account> getAccount(OfflinePlayer player, World world);
+    default CompletableFuture<Account> loadAccount(OfflinePlayer player, World world) {
+        return loadAccount(player.getUniqueId(), world);
+    }
 
     /**
-     * Retrieves the account with the specified unique ID.
+     * Loads the account with the specified uuid asynchronously.
      *
-     * @param uuid the unique ID of the account to be retrieved
+     * @param uuid the uuid of the account to be retrieved
      * @return a CompletableFuture that will complete with the retrieved account
      */
-    CompletableFuture<Account> getAccount(UUID uuid);
+    CompletableFuture<Account> loadAccount(UUID uuid);
 
     /**
-     * Retrieves the account for the specified unique ID and world.
+     * Loads the account for the specified uuid and world asynchronously.
      *
-     * @param uuid  the unique ID of the account to be retrieved
+     * @param uuid  the uuid of the account to be retrieved
      * @param world the world in which the account is located
      * @return a CompletableFuture that will complete with the retrieved account
      */
-    CompletableFuture<Account> getAccount(UUID uuid, World world);
+    CompletableFuture<Account> loadAccount(UUID uuid, World world);
 
     /**
      * Deletes the specified account.
@@ -123,7 +218,11 @@ public interface EconomyController {
      * @param account the account to be deleted
      * @return a CompletableFuture that will complete when the account is deleted
      */
-    CompletableFuture<Boolean> deleteAccount(Account account);
+    default CompletableFuture<Boolean> deleteAccount(Account account) {
+        return account.getWorld()
+                .map(world -> deleteAccount(account.getOwner(), world))
+                .orElseGet(() -> deleteAccount(account.getOwner()));
+    }
 
     /**
      * Deletes the account of the specified player.
@@ -131,7 +230,9 @@ public interface EconomyController {
      * @param player the player whose account will be deleted
      * @return a CompletableFuture that will complete when the account is deleted
      */
-    CompletableFuture<Boolean> deleteAccount(OfflinePlayer player);
+    default CompletableFuture<Boolean> deleteAccount(OfflinePlayer player) {
+        return deleteAccount(player.getUniqueId());
+    }
 
     /**
      * Deletes the account of the specified player in the specified world.
@@ -140,20 +241,22 @@ public interface EconomyController {
      * @param world  the world in which the player's account exists
      * @return a CompletableFuture that will complete when the account is deleted
      */
-    CompletableFuture<Boolean> deleteAccount(OfflinePlayer player, World world);
+    default CompletableFuture<Boolean> deleteAccount(OfflinePlayer player, World world) {
+        return deleteAccount(player.getUniqueId(), world);
+    }
 
     /**
-     * Deletes the account with the specified unique ID.
+     * Deletes the account with the specified uuid.
      *
-     * @param uuid the unique ID of the account to be deleted
+     * @param uuid the uuid of the account to be deleted
      * @return a CompletableFuture that will complete when the account is deleted
      */
     CompletableFuture<Boolean> deleteAccount(UUID uuid);
 
     /**
-     * Deletes the account with the specified unique ID in the specified world.
+     * Deletes the account with the specified uuid in the specified world.
      *
-     * @param uuid  the unique ID of the account to be deleted
+     * @param uuid  the uuid of the account to be deleted
      * @param world the world in which the account exists
      * @return a CompletableFuture that will complete when the account is deleted
      */
@@ -165,4 +268,12 @@ public interface EconomyController {
      * @return the number of fractional digits used for formatting currency amounts
      */
     int fractionalDigits();
+
+    /**
+     * Retrieves the name associated with the economy controller.
+     *
+     * @return the name of the economy controller.
+     */
+    @Contract(pure = true)
+    String getName();
 }
