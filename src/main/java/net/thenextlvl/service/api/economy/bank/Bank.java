fd50a095180a1ef5f96647f497937d2b6d93b777
david <mrminecraft00@gmail.com>
2024-08-25 09:10:10 +0200

    Add member management functionality to Bank interface
    Introduced methods for adding, checking, and removing members in the Bank interface, as well as setting an owner. These methods include both UUID and OfflinePlayer variants for flexibility. Extending the Bank interface from Account enhances its functionality.

--- src/main/java/net/thenextlvl/service/api/economy/bank/Bank.java
+++ src/main/java/net/thenextlvl/service/api/economy/bank/Bank.java
@@ -1,5 +1,37 @@
 package net.thenextlvl.service.api.economy.bank;
 
-public interface Bank {
+import net.thenextlvl.service.api.economy.Account;
+import org.bukkit.OfflinePlayer;
+
+import java.util.Set;
+import java.util.UUID;
+
+public interface Bank extends Account {
+    Set<UUID> getMembers();
+
     String getName();
+
+    default boolean addMember(OfflinePlayer player) {
+        return addMember(player.getUniqueId());
+    }
+
+    boolean addMember(UUID uuid);
+
+    default boolean isMember(OfflinePlayer player) {
+        return isMember(player.getUniqueId());
+    }
+
+    boolean isMember(UUID uuid);
+
+    default boolean removeMember(OfflinePlayer player) {
+        return removeMember(player.getUniqueId());
+    }
+
+    boolean removeMember(UUID uuid);
+
+    default boolean setOwner(OfflinePlayer player) {
+        return setOwner(player.getUniqueId());
+    }
+
+    boolean setOwner(UUID uuid);
 }
