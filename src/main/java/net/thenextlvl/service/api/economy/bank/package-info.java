1b9cb1d665dc3130600a3143105ccdedd27020b6
david <mrminecraft00@gmail.com>
2024-08-25 09:10:25 +0200

    Add null-safety annotations to bank package
    Introduced a new package-info.java file for the bank package to enforce null-safety annotations. This ensures fields, methods, parameters, and types within the package are not null by default, improving code reliability and safety.

---
+++ src/main/java/net/thenextlvl/service/api/economy/bank/package-info.java
@@ -0,0 +1,10 @@
+@TypesAreNotNullByDefault
+@FieldsAreNotNullByDefault
+@MethodsReturnNotNullByDefault
+@ParametersAreNotNullByDefault
+package net.thenextlvl.service.api.economy.bank;
+
+import core.annotation.FieldsAreNotNullByDefault;
+import core.annotation.MethodsReturnNotNullByDefault;
+import core.annotation.ParametersAreNotNullByDefault;
+import core.annotation.TypesAreNotNullByDefault;
