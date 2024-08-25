d86acbd77db70b8577b09808adb71acec7319123
david <mrminecraft00@gmail.com>
2024-08-25 09:06:36 +0200

    Add default nullability annotations
    Introduced @TypesAreNotNullByDefault and related annotations to `service.api.node` package and specified nullability for `service.wrapper` package. This ensures stronger type checks and reduces nullability-related issues.

---
+++ plugin/src/main/java/net/thenextlvl/service/wrapper/package-info.java
@@ -0,0 +1,8 @@
+@FieldsAreNotNullByDefault
+@MethodsReturnNotNullByDefault
+@ParametersAreNullableByDefault
+package net.thenextlvl.service.wrapper;
+
+import core.annotation.FieldsAreNotNullByDefault;
+import core.annotation.MethodsReturnNotNullByDefault;
+import core.annotation.ParametersAreNullableByDefault;
