fb0987d796d13f65d39173932ec30d08f52ead1a
david <mrminecraft00@gmail.com>
2024-08-25 09:06:21 +0200

    Refactor ServicePlugin initialization and logging
    Reorganize and streamline service loading methods, adding null checks and improving logging consistency. Introduce the printServices method to log the current service providers.

--- plugin/src/main/java/net/thenextlvl/service/ServicePlugin.java
+++ plugin/src/main/java/net/thenextlvl/service/ServicePlugin.java
@@ -1,5 +1,6 @@
 package net.thenextlvl.service;
 
+import com.google.common.base.Preconditions;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 import net.milkbowl.vault.chat.Chat;
@@ -25,6 +26,7 @@ import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
 import java.util.concurrent.Callable;
 import java.util.function.Function;
 
@@ -39,7 +41,7 @@ public class ServicePlugin extends JavaPlugin {
 
     private @Nullable VaultPermissionServiceWrapper vaultPermissionWrapper = null;
 
-    private PermissionController permissionController = new SuperPermsPermissionController();
+    private PermissionController permissionController = new SuperPermsPermissionController(this);
 
     @Override
     public void onLoad() {
@@ -51,6 +53,8 @@ public class ServicePlugin extends JavaPlugin {
         loadPermissionServices();
         loadGroupServices();
         loadChatServices();
+
+        printServices();
 
         loadVaultPermissionWrapper();
         loadVaultEconomyWrapper();
@@ -64,35 +68,27 @@ public class ServicePlugin extends JavaPlugin {
         metrics().shutdown();
     }
 
-    private void addCustomCharts() {
-        addCustomChart(Chat.class, Chat::getName, "chat");
-        addCustomChart(Economy.class, Economy::getName, "economy");
-        addCustomChart(Permission.class, Permission::getName, "permission");
-    }
-
-    private <T> void addCustomChart(Class<T> service, Function<T, String> function, String name) {
-        var loaded = getServer().getServicesManager().load(service);
-        metrics.addCustomChart(new SimplePie(name, () -> loaded != null ? function.apply(loaded) : "None"));
-    }
-
+    @SuppressWarnings("Convert2MethodRef")
     private void loadChatServices() {
-        hookChatService("GroupManager", GroupManagerChatController::new, ServicePriority.High);
-        hookChatService("LuckPerms", LuckPermsChatController::new, ServicePriority.Highest);
+        hookChatService("GroupManager", () -> new GroupManagerChatController(), ServicePriority.High);
+        hookChatService("LuckPerms", () -> new LuckPermsChatController(), ServicePriority.Highest);
         this.chatController = getServer().getServicesManager().load(ChatController.class);
     }
 
+    @SuppressWarnings("Convert2MethodRef")
     private void loadGroupServices() {
-        hookGroupService("GroupManager", GroupManagerGroupController::new, ServicePriority.High);
-        hookGroupService("LuckPerms", LuckPermsGroupController::new, ServicePriority.Highest);
+        hookGroupService("GroupManager", () -> new GroupManagerGroupController(), ServicePriority.High);
+        hookGroupService("LuckPerms", () -> new LuckPermsGroupController(), ServicePriority.Highest);
         this.groupController = getServer().getServicesManager().load(GroupController.class);
     }
 
+    @SuppressWarnings("Convert2MethodRef")
     private void loadPermissionServices() {
-        hookPermissionService("GroupManager", GroupManagerPermissionController::new, ServicePriority.High);
-        hookPermissionService("LuckPerms", LuckPermsPermissionController::new, ServicePriority.Highest);
+        hookPermissionService("GroupManager", () -> new GroupManagerPermissionController(), ServicePriority.High);
+        hookPermissionService("LuckPerms", () -> new LuckPermsPermissionController(), ServicePriority.Highest);
 
         getServer().getServicesManager().register(PermissionController.class, permissionController, this, ServicePriority.Lowest);
-        getComponentLogger().info("Added SuperPerms as backup permission provider (Lowest)");
+        getComponentLogger().debug("Added SuperPerms as backup permission provider (Lowest)");
 
         var controller = getServer().getServicesManager().load(PermissionController.class);
         if (controller != null) this.permissionController = controller;
@@ -103,7 +99,7 @@ public class ServicePlugin extends JavaPlugin {
             if (getServer().getPluginManager().getPlugin(name) == null) return;
             var hook = callable.call();
             getServer().getServicesManager().register(ChatController.class, hook, this, priority);
-            getComponentLogger().info("Added {} as chat provider ({})", name, priority.name());
+            getComponentLogger().debug("Added {} as chat provider ({})", name, priority.name());
         } catch (Exception e) {
             getComponentLogger().error("Failed to register {} as chat provider - " +
                                        "check to make sure you're using a compatible version!", name, e);
@@ -115,7 +111,7 @@ public class ServicePlugin extends JavaPlugin {
             if (getServer().getPluginManager().getPlugin(name) == null) return;
             var hook = callable.call();
             getServer().getServicesManager().register(GroupController.class, hook, this, priority);
-            getComponentLogger().info("Added {} as group provider ({})", name, priority.name());
+            getComponentLogger().debug("Added {} as group provider ({})", name, priority.name());
         } catch (Exception e) {
             getComponentLogger().error("Failed to register {} as group provider - " +
                                        "check to make sure you're using a compatible version!", name, e);
@@ -127,7 +123,7 @@ public class ServicePlugin extends JavaPlugin {
             if (getServer().getPluginManager().getPlugin(name) == null) return;
             var hook = callable.call();
             getServer().getServicesManager().register(PermissionController.class, hook, this, priority);
-            getComponentLogger().info("Added {} as permission provider ({})", name, priority.name());
+            getComponentLogger().debug("Added {} as permission provider ({})", name, priority.name());
         } catch (Exception e) {
             getComponentLogger().error("Failed to register {} as permission provider - " +
                                        "check to make sure you're using a compatible version!", name, e);
@@ -146,7 +142,48 @@ public class ServicePlugin extends JavaPlugin {
     }
 
     private void loadVaultChatWrapper() {
-        var wrapper = new VaultChatServiceWrapper(vaultPermissionWrapper(), chatController(), this);
+        if (chatController() == null) return;
+        Preconditions.checkNotNull(vaultPermissionWrapper(), "vault permission wrapper cannot be null");
+        var wrapper = new VaultChatServiceWrapper(vaultPermissionWrapper(), groupController(), chatController(), this);
         getServer().getServicesManager().register(Chat.class, wrapper, this, ServicePriority.Highest);
+    }
+
+    private void printServices() {
+        var chat = chatController() != null ? chatController().getName() : null;
+        var group = groupController() != null ? groupController().getName() : null;
+        var permission = permissionController().getName();
+
+        if (chat == null && group == null) {
+            getComponentLogger().info("Found no chat and group provider");
+        } else if (chat == null) {
+            getComponentLogger().info("Found no chat provider");
+        } else if (group == null) {
+            getComponentLogger().info("Found no group provider");
+        }
+
+        if (Objects.equals(chat, group) && Objects.equals(chat, permission)) {
+            getComponentLogger().info("Using {} as chat, group and permission provider", chat);
+        } else if (chat != null && Objects.equals(chat, group)) {
+            getComponentLogger().info("Using {} as chat and group provider", chat);
+            getComponentLogger().info("Using {} as permission provider", permission);
+        } else if (Objects.equals(chat, permission)) {
+            getComponentLogger().info("Using {} as chat and permission provider", chat);
+            if (group != null) getComponentLogger().info("Using {} as group provider", group);
+        } else {
+            if (chat != null) getComponentLogger().info("Using {} as chat provider", chat);
+            if (group != null) getComponentLogger().info("Using {} as group provider", group);
+            getComponentLogger().info("Using {} as permission provider", permission);
+        }
+    }
+
+    private void addCustomCharts() {
+        addCustomChart(Chat.class, Chat::getName, "chat");
+        addCustomChart(Economy.class, Economy::getName, "economy");
+        addCustomChart(Permission.class, Permission::getName, "permission");
+    }
+
+    private <T> void addCustomChart(Class<T> service, Function<T, String> function, String name) {
+        var loaded = getServer().getServicesManager().load(service);
+        metrics.addCustomChart(new SimplePie(name, () -> loaded != null ? function.apply(loaded) : "None"));
     }
 }
