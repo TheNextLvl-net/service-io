f565903a1c2445f162b40752fda5fa7d408555ba
david <mrminecraft00@gmail.com>
2024-08-28 22:51:30 +0200

    Add WrappedAccount class implementation
    Introduce the WrappedAccount class, which implements the Account interface. This class uses the Vault Economy API to handle deposit, withdraw, and balance retrieval operations, with support for optional world-specific context.

---
+++ plugin/src/main/java/net/thenextlvl/service/wrapper/service/model/WrappedAccount.java
@@ -0,0 +1,54 @@
+package net.thenextlvl.service.wrapper.service.model;
+
+import lombok.RequiredArgsConstructor;
+import net.milkbowl.vault.economy.Economy;
+import net.thenextlvl.service.api.economy.Account;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class WrappedAccount implements Account {
+    private final @Nullable World world;
+    private final Economy economy;
+    private final OfflinePlayer holder;
+
+    @Override
+    public BigDecimal deposit(Number amount) {
+        var response = economy.depositPlayer(holder, world != null ? world.getName() : null, amount.doubleValue());
+        return new BigDecimal(response.balance);
+    }
+
+    @Override
+    public BigDecimal getBalance() {
+        var balance = economy.getBalance(holder, world != null ? world.getName() : null);
+        return new BigDecimal(balance);
+    }
+
+    @Override
+    public BigDecimal withdraw(Number amount) {
+        var response = economy.withdrawPlayer(holder, world != null ? world.getName() : null, amount.doubleValue());
+        return new BigDecimal(response.balance);
+    }
+
+    @Override
+    public Optional<World> getWorld() {
+        return Optional.ofNullable(world);
+    }
+
+    @Override
+    public UUID getOwner() {
+        return holder.getUniqueId();
+    }
+
+    @Override
+    public void setBalance(Number balance) {
+        var difference = balance.doubleValue() - getBalance().doubleValue();
+        if (difference > 0) deposit(difference);
+        else if (difference < 0) withdraw(-difference);
+    }
+}
