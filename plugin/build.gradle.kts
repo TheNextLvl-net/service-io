a1f1e554a3a4169a4d84a0f53cd976fedbd44576
david <mrminecraft00@gmail.com>
2024-10-03 13:44:17 +0200

    Update net.thenextlvl.core dependency version
    This commit updates the net.thenextlvl.core dependency from version 1.5.1 to 1.5.2 in the build script. This update likely includes important bug fixes or improvements. Ensure compatibility and test thoroughly after this change.

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     compileOnly("net.luckperms:api:5.4")
 
     implementation("com.github.MilkBowl:VaultAPI:1.7.1")
-    implementation("net.thenextlvl.core:paper:1.5.1")
+    implementation("net.thenextlvl.core:paper:1.5.2")
     implementation("org.bstats:bstats-bukkit:3.1.0")
 
     implementation(rootProject)
