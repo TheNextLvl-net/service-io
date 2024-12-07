209b1a873ac42ab6249c45479127bc5bcc23401c
david <mrminecraft00@gmail.com>
2024-12-07 17:13:14 +0100

    Refactor Viewable interface to improve visibility control
    Reorder methods in the Viewable interface and add new visibility-related methods. Introduce methods to handle object visibility state and display range management such as isInvisible, setVisibleByDefault, and setDisplayRange. This enhances control over how viewership is managed for objects implementing this interface.

--- src/main/java/net/thenextlvl/service/api/model/Viewable.java
+++ src/main/java/net/thenextlvl/service/api/model/Viewable.java
@@ -9,29 +9,33 @@ import java.util.Set;
 
 @NullMarked
 public interface Viewable {
-    // add the given player to the viewers of this object
-    boolean addViewer(Player player);
-
-    // add all given players to the viewers of this object
-    boolean addViewers(Collection<Player> player);
-
-    // remove this object for the given player
-    boolean removeViewer(Player player);
-
-    // remove this object for all given players
-    boolean removeViewers(Collection<Player> player);
-
-    // all players that can actively see this object
     @Unmodifiable
     Set<Player> getTrackedBy();
 
-    // all players this object should be sent to
     @Unmodifiable
     Set<Player> getViewers();
 
-    // whether the object should be sent to the player in the first place
+    boolean addViewer(Player player);
+
+    boolean addViewers(Collection<Player> player);
+
+    boolean isInvisible();
+
+    boolean isTrackedBy(Player player);
+
     boolean isViewer(Player player);
 
-    // whether the object is currently visible to the player
-    boolean isTrackedBy(Player player);
+    boolean isVisibleByDefault();
+
+    boolean removeViewer(Player player);
+
+    boolean removeViewers(Collection<Player> player);
+
+    double getDisplayRange();
+
+    void setDisplayRange(double range);
+
+    void setInvisible(boolean invisible);
+
+    void setVisibleByDefault(boolean visible);
 }
