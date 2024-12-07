af74a15ecbf9e424352a46baf2e46bd16e4027a9
david <mrminecraft00@gmail.com>
2024-12-07 16:22:05 +0100

    Remove redundant profile methods from PlayerCharacter
    The PlayerCharacter interface had redundant getProfile and setProfile methods that are already available in the Player interface. This change streamlines the interface by removing unnecessary methods, reducing potential confusion and maintenance effort.

--- src/main/java/net/thenextlvl/service/api/npc/PlayerCharacter.java
+++ src/main/java/net/thenextlvl/service/api/npc/PlayerCharacter.java
@@ -1,16 +1,11 @@
 package net.thenextlvl.service.api.npc;
 
-import com.destroystokyo.paper.profile.PlayerProfile;
 import org.bukkit.entity.Player;
 import org.jspecify.annotations.NullMarked;
 
 @NullMarked
 public interface PlayerCharacter extends Character, Player {
-    PlayerProfile getProfile();
-
     boolean isTablistEntryHidden();
-
-    void setProfile(PlayerProfile profile);
 
     void setTablistEntryHidden(boolean hidden);
 }
