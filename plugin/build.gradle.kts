631364c73ba14da7d7203aef72da5e86e9198449
david <github@nonswag.dev>
2025-06-19 06:54:03 +0200

    Add changelog handling for deployments
    Passed changelog via environment variables for Modrinth and Hangar. Updated workflows to support changelog integration. Added README syncing for Modrinth.

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -134,6 +134,7 @@ hangarPublish { // docs - https://docs.papermc.io/misc/hangar-publishing
     publications.register("plugin") {
         id.set("ServiceIO")
         version.set(versionString)
+        changelog = System.getenv("CHANGELOG")
         channel.set(if (isRelease) "Release" else "Snapshot")
         apiKey.set(System.getenv("HANGAR_API_TOKEN"))
         platforms.register(Platforms.PAPER) {
@@ -146,9 +147,11 @@ hangarPublish { // docs - https://docs.papermc.io/misc/hangar-publishing
 modrinth {
     token.set(System.getenv("MODRINTH_TOKEN"))
     projectId.set("MNPyHOe7")
+    changelog = System.getenv("CHANGELOG")
     versionType = if (isRelease) "release" else "beta"
     uploadFile.set(tasks.shadowJar)
     gameVersions.set(versions)
+    syncBodyFrom.set(rootProject.file("README.md").readText())
     loaders.add("paper")
     loaders.add("folia")
     dependencies {
