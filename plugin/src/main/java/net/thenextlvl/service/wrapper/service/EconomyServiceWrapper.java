59b9368a8856290a8df19507762e87cfe512805e
david <github@nonswag.dev>
2025-07-15 20:00:49 +0200

    Added support for nullable `World` parameters
    Refactored `EconomyController` and `BankController` methods to include `@Nullable` `World` parameters. Adjusted method signatures and default behaviors to accommodate this change.

--- plugin/src/main/java/net/thenextlvl/service/wrapper/service/EconomyServiceWrapper.java
+++ plugin/src/main/java/net/thenextlvl/service/wrapper/service/EconomyServiceWrapper.java
@@ -3,16 +3,18 @@ package net.thenextlvl.service.wrapper.service;
 import net.milkbowl.vault.economy.Economy;
 import net.thenextlvl.service.ServicePlugin;
 import net.thenextlvl.service.api.economy.Account;
-import net.thenextlvl.service.api.economy.currency.Currency;
 import net.thenextlvl.service.api.economy.EconomyController;
+import net.thenextlvl.service.api.economy.currency.Currency;
 import net.thenextlvl.service.wrapper.service.model.WrappedAccount;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.Unmodifiable;
 import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
@@ -34,90 +36,67 @@ public class EconomyServiceWrapper implements EconomyController {
     }
 
     @Override
-    public CompletableFuture<@Unmodifiable Set<Account>> loadAccounts() {
-        return CompletableFuture.completedFuture(getAccounts());
+    public CompletableFuture<@Unmodifiable Set<Account>> loadAccounts(@Nullable World world) {
+        return CompletableFuture.completedFuture(getAccounts(world));
     }
 
     @Override
-    public @Unmodifiable Set<Account> getAccounts() {
+    public @Unmodifiable Set<Account> getAccounts(@Nullable World world) {
         return Arrays.stream(plugin.getServer().getOfflinePlayers())
-                .filter(economy::hasAccount)
-                .map(player -> new WrappedAccount(this, null, economy, player))
+                .filter(player -> economy.hasAccount(player, world != null ? world.getName() : null))
+                .map(player -> new WrappedAccount(this, world, economy, player))
                 .collect(Collectors.toUnmodifiableSet());
     }
 
     @Override
-    public Optional<Account> getAccount(OfflinePlayer player) {
-        if (!economy.hasAccount(player)) return Optional.empty();
-        return Optional.of(new WrappedAccount(this, null, economy, player));
-    }
-
-    @Override
-    public Optional<Account> getAccount(OfflinePlayer player, World world) {
-        if (!economy.hasAccount(player, world.getName())) return Optional.empty();
+    public Optional<Account> getAccount(OfflinePlayer player, @Nullable World world) {
+        if (!economy.hasAccount(player, world != null ? world.getName() : null)) return Optional.empty();
         return Optional.of(new WrappedAccount(this, world, economy, player));
     }
 
     @Override
-    public Optional<Account> getAccount(UUID uuid) {
-        return getAccount(plugin.getServer().getOfflinePlayer(uuid));
-    }
-
-    @Override
-    public Optional<Account> getAccount(UUID uuid, World world) {
+    public Optional<Account> getAccount(UUID uuid, @Nullable World world) {
         return getAccount(plugin.getServer().getOfflinePlayer(uuid), world);
     }
 
     @Override
-    public CompletableFuture<Account> createAccount(OfflinePlayer player) {
-        return CompletableFuture.completedFuture(economy.createPlayerAccount(player))
-                .thenApply(account -> getAccount(player).orElseThrow());
+    public CompletableFuture<Account> createAccount(OfflinePlayer player, @Nullable World world) {
+        var created = economy.createPlayerAccount(player, world != null ? world.getName() : null);
+        if (created) loadAccount(player, world).thenApply(account -> account.orElseThrow(() ->
+                new IllegalStateException("Could not find player account after creation")));
+        return CompletableFuture.failedFuture(new IllegalStateException(
+                "Similar account already exists"
+        ));
     }
 
     @Override
-    public CompletableFuture<Account> createAccount(OfflinePlayer player, World world) {
-        return CompletableFuture.completedFuture(economy.createPlayerAccount(player, world.getName()))
-                .thenApply(account -> getAccount(player, world).orElseThrow());
-    }
-
-    @Override
-    public CompletableFuture<Account> createAccount(UUID uuid) {
-        return createAccount(plugin.getServer().getOfflinePlayer(uuid));
-    }
-
-    @Override
-    public CompletableFuture<Account> createAccount(UUID uuid, World world) {
+    public CompletableFuture<Account> createAccount(UUID uuid, @Nullable World world) {
         return createAccount(plugin.getServer().getOfflinePlayer(uuid), world);
     }
 
     @Override
-    public CompletableFuture<Optional<Account>> loadAccount(OfflinePlayer player) {
-        return CompletableFuture.completedFuture(getAccount(player));
-    }
-
-    @Override
-    public CompletableFuture<Optional<Account>> loadAccount(OfflinePlayer player, World world) {
+    public CompletableFuture<Optional<Account>> loadAccount(OfflinePlayer player, @Nullable World world) {
         return CompletableFuture.completedFuture(getAccount(player, world));
     }
 
     @Override
-    public CompletableFuture<Optional<Account>> loadAccount(UUID uuid) {
-        return loadAccount(plugin.getServer().getOfflinePlayer(uuid));
-    }
-
-    @Override
-    public CompletableFuture<Optional<Account>> loadAccount(UUID uuid, World world) {
+    public CompletableFuture<Optional<Account>> loadAccount(UUID uuid, @Nullable World world) {
         return loadAccount(plugin.getServer().getOfflinePlayer(uuid), world);
     }
 
     @Override
-    public CompletableFuture<Boolean> deleteAccount(UUID uuid) {
+    public CompletableFuture<Boolean> deleteAccount(UUID uuid, @Nullable World world) {
         return CompletableFuture.completedFuture(false);
     }
 
     @Override
-    public CompletableFuture<Boolean> deleteAccount(UUID uuid, World world) {
+    public CompletableFuture<Boolean> deleteAccounts(List<UUID> accounts, @Nullable World world) {
         return CompletableFuture.completedFuture(false);
+    }
+
+    @Override
+    public boolean hasMultiWorldSupport() {
+        return false;
     }
 
     @Override
