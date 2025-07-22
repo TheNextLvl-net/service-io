925174857a9ba795ddeda00e56f9e48864530430
david <github@nonswag.dev>
2025-07-22 20:44:31 +0200

    Added `canDeposit`, `canWithdraw`, and `canHold` methods
    Enhanced `Bank` and `Account` APIs with balance operation checks. Implemented default behaviors and updated wrappers to align with the new methods for consistent validation logic.

--- plugin/src/main/java/net/thenextlvl/service/wrapper/service/model/WrappedBank.java
+++ plugin/src/main/java/net/thenextlvl/service/wrapper/service/model/WrappedBank.java
@@ -71,6 +71,11 @@ public class WrappedBank implements Bank {
     }
 
     @Override
+    public boolean canHold(Currency currency) {
+        return false;
+    }
+
+    @Override
     public @Unmodifiable Set<UUID> getMembers() {
         return Arrays.stream(provider.getServer().getOfflinePlayers())
                 .filter(player -> economy.isBankMember(name, player).transactionSuccess())
@@ -101,5 +106,25 @@ public class WrappedBank implements Bank {
     @Override
     public boolean setOwner(UUID uuid) {
         return false;
+    }
+
+    @Override
+    public boolean canDeposit(OfflinePlayer player, Number amount, Currency currency) {
+        return economy.isBankOwner(getName(), player).transactionSuccess();
+    }
+
+    @Override
+    public boolean canDeposit(UUID uuid, Number amount, Currency currency) {
+        return canDeposit(plugin.getServer().getOfflinePlayer(uuid), amount, currency);
+    }
+
+    @Override
+    public boolean canWithdraw(OfflinePlayer player, Number amount, Currency currency) {
+        return economy.isBankOwner(getName(), player).transactionSuccess();
+    }
+
+    @Override
+    public boolean canWithdraw(UUID uuid, Number amount, Currency currency) {
+        return canWithdraw(plugin.getServer().getOfflinePlayer(uuid), amount, currency);
     }
 }
