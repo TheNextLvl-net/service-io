a828d7bd106400b7710b65558eb8decfebf8fa1f
david <mail@nonswag.dev>
2025-09-30 19:46:40 +0200

    Update Paper API to 1.21.9-R0.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.9-R0.1-SNAPSHOT")
 }
 
 publishing {
