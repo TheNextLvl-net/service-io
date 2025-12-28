5fac140a4736177a21e9d02befc4af944f967e80
david <mail@nonswag.dev>
2025-12-28 07:56:02 +0100

    Refactor hologram models for v2 and v3 support

---
+++ fancy-holograms-v2/build.gradle.kts
@@ -0,0 +1,7 @@
+repositories {
+    maven("https://repo.fancyplugins.de/releases")
+}
+
+dependencies {
+    compileOnly("de.oliver:FancyHolograms:2.9.0")
+}
---
+++ fancy-holograms-v3/build.gradle.kts
@@ -0,0 +1,8 @@
+repositories {
+    maven("https://repo.fancyplugins.de/releases")
+    maven("https://repo.fancyplugins.de/snapshots")
+}
+
+dependencies {
+    compileOnly("de.oliver:FancyHolograms:3.0.0-SNAPSHOT.4")
+}
