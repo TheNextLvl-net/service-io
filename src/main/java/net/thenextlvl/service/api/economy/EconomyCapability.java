32d782778868f68aa4670900345b845af6abe5ae
david <mail@nonswag.dev>
2026-03-30 19:49:48 +0200

    Added `CURRENCY_CREATION` to `EconomyCapability` enum
    Introduced a new capability to indicate support for creating currencies. Updated Javadocs for clarity and noted dependency on `MULTI_CURRENCY`.

--- src/main/java/net/thenextlvl/service/api/economy/EconomyCapability.java
+++ src/main/java/net/thenextlvl/service/api/economy/EconomyCapability.java
@@ -13,6 +13,14 @@ public enum EconomyCapability implements Capability {
      */
     MULTI_CURRENCY,
     /**
+     * The provider supports creating new currencies.
+     * <p>
+     * If a provider supports currency creation, {@link #MULTI_CURRENCY} is also supported.
+     *
+     * @since 3.0.0
+     */
+    CURRENCY_CREATION,
+    /**
      * The provider supports per-world economies.
      */
     MULTI_WORLD
